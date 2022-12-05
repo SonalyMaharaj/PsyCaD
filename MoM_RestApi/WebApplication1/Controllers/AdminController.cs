610e0e635e03f6c086aa50119045c7bac0f78f80
Solomon <peacefulmoyo7@gmail.com>
2022-12-05 13:49:38 +0200

    api created

---
+++ MoM_RestApi/WebApplication1/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class AdminController : ApiController
+    {
+
+       
+
+    }
+}
